255d26c6d3bd12638cc2574d1061861cb1d0f574
ipoemi <ipoemi@nate.com>
2018-04-26 11:19:12 +0900

    Breaking the Records Added

---
+++ src/main/scala/ipoemi/hackerrank/practice/algorithms/implementation/breakingtherecords/Solution.scala
@@ -0,0 +1,36 @@
+package ipoemi.hackerrank.practice.algorithms.implementation.breakingtherecords
+
+import java.io.PrintWriter
+
+object Solution {
+
+  /*
+   * Complete the breakingRecords function below.
+   */
+  def breakingRecords(score: Array[Int]): Array[Int] = {
+    /*
+     * Write your code here.
+     */
+    val (_, minCnt0, _, maxCnt0) = score.tail.foldLeft((score.head, 0, score.head, 0)) { case ((min, minCnt, max, maxCnt), s) =>
+      if (s < min) (s, minCnt + 1, max, maxCnt)
+      else if (s > max) (min, minCnt, s, maxCnt + 1)
+      else (min, minCnt, max, maxCnt)
+    }
+    Array(maxCnt0, minCnt0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val scan = scala.io.StdIn
+
+    val fw = new PrintWriter(sys.env("OUTPUT_PATH"))
+
+    val _ = scan.readLine.trim.toInt
+
+    val score = scan.readLine.split(" ").map(_.trim.toInt)
+    val result = breakingRecords(score)
+
+    fw.println(result.mkString(" "))
+
+    fw.close()
+  }
+}
