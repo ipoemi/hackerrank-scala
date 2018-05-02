bce795e4a12bf41b0af0be94bdc8c2a5a67e0293
ipoemi <ipoemi@nate.com>
2018-05-02 22:53:24 +0900

    Migratory Birds Added

---
+++ src/test/scala/ipoemi/hackerrank/practice/algorithms/implementation/migratorybirds/SolutionSpec.scala
@@ -0,0 +1,18 @@
+package ipoemi.hackerrank.practice.algorithms.implementation.migratorybirds
+
+import org.scalatest._
+
+class SolutionSpec extends WordSpecLike with Matchers {
+
+  "Solution" must {
+
+    "TestCase #0" in {
+      val expect = 4
+      val sol = Solution.migratoryBirds(6, Array(1, 4, 4, 4, 5, 3))
+
+      sol shouldBe expect
+    }
+  }
+
+}
+
