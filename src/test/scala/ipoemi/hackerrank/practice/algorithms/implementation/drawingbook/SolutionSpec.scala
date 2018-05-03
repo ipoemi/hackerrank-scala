d316048d2f7418b5520e4fea779c02e2c1b333a4
ipoemi <ipoemi@nate.com>
2018-05-03 22:16:47 +0900

    Drawing Book Added

---
+++ src/test/scala/ipoemi/hackerrank/practice/algorithms/implementation/drawingbook/SolutionSpec.scala
@@ -0,0 +1,26 @@
+package ipoemi.hackerrank.practice.algorithms.implementation.drawingbook
+
+import org.scalatest._
+
+class SolutionSpec extends WordSpecLike with Matchers {
+
+  "Solution" must {
+
+    "TestCase #0" in {
+      val expect = 1
+      val sol = Solution.pageCount(6, 2)
+
+      sol shouldBe expect
+    }
+
+    "TestCase #1" in {
+      val expect = 0
+      val sol = Solution.pageCount(5, 4)
+
+      sol shouldBe expect
+    }
+
+  }
+
+}
+
