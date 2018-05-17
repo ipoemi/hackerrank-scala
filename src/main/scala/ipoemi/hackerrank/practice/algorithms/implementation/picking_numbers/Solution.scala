a67c96a8de5c747d7d343519c79a4eb65f7fd72c
ipoemi <ipoemi@nate.com>
2018-05-17 15:52:20 +0900

    Picking Numbers - Other Solution Added

--- src/main/scala/ipoemi/hackerrank/practice/algorithms/implementation/picking_numbers/Solution.scala
+++ src/main/scala/ipoemi/hackerrank/practice/algorithms/implementation/picking_numbers/Solution.scala
@@ -3,11 +3,12 @@ package ipoemi.hackerrank.practice.algorithms.implementation.picking_numbers
 import java.io.PrintWriter
 
 import scala.annotation.tailrec
+import scala.collection.immutable._
 
 object Solution {
 
   // Complete the pickingNumbers function below.
-  def pickingNumbers(a: Array[Int]): Int = sol2(a)
+  def pickingNumbers(a: Array[Int]): Int = sol3(a)
 
   def sol1(a: Array[Int]): Int = {
     val sorted = a.sorted.toList
@@ -37,6 +38,15 @@ object Solution {
       val length = remain.size - diffLessOne.size
       (maxLength max length, diffZero)
     }._1
+  }
+
+  def sol3(a: Array[Int]): Int = {
+    val cache = a.foldLeft(Map.empty[Int, Int]) { (m, k) =>
+      m.updated(k, m.getOrElse(k, 0) + 1)
+    }
+    cache.keys.foldLeft(0) { (maxLength, i) =>
+      maxLength max cache.getOrElse(i, 0) + cache.getOrElse(i + 1, 0)
+    }
   }
 
   def main(args: Array[String]): Unit = {
